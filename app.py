a11f8ee7acf1d5790f6fa035fcc4e7d153982395
Kenneth Woanyah <kenneth.woanyah@gmail.com>
2023-11-24 13:51:20 -0500

    streamlit upload

--- app.py
+++ app.py
@@ -23,3 +23,43 @@ st.text('Fixed width text')
 
 
 
+
+# Assuming other necessary imports based on the Jupyter notebook's content
+
+# Define the main function of the Streamlit app
+def main():
+    st.header("Welcome to FiniBot Financial Advisory! Please upload your financial spreadsheet. [Link to template]")
+
+    # File uploader for the spreadsheet
+    uploaded_file = st.file_uploader("Upload spreadsheet", type=['csv', 'xlsx'])
+    
+    # Radio button for selecting experience level
+    experience_level = st.radio("Select your experience level:", ("Novice", "Expert"))
+
+    # Display the spreadsheet if uploaded
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)  # Adjust this line if your spreadsheet is in a different format
+        st.dataframe(df)  # Displaying the spreadsheet
+
+        # Process the spreadsheet and generate financial analysis
+        analysis, recommendation = process_spreadsheet(df, experience_level)
+        
+        # Display the analysis and recommendation
+        st.markdown("### FiniBot Analysis and Recommendation")
+        st.markdown(f"**Analysis:**
+{analysis}")
+        st.markdown(f"**Recommendation:**
+{recommendation}")
+
+# Function to process the spreadsheet and generate analysis and recommendation
+# This is a placeholder function, you need to integrate the logic from your Jupyter notebook here.
+def process_spreadsheet(df, experience_level):
+    # Process the dataframe as per the logic in the Jupyter notebook
+    # For example, calculate total savings, monthly debt, and monthly income, etc.
+    # Then, based on these calculations and the experience level, generate the analysis and recommendation.
+    analysis = "Analysis based on the spreadsheet data."
+    recommendation = "Recommendation based on the analysis."
+    return analysis, recommendation
+
+if __name__ == "__main__":
+    main()
