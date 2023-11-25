5ab78c258c68a20442798131ab5385fd5ee8a5cf
Kenneth Woanyah <kenneth.woanyah@gmail.com>
2023-11-25 00:03:35 -0500

    update

--- app.py
+++ app.py
@@ -1,86 +1,189 @@
-# Import necessary libraries
 import streamlit as st
-import openai
 import os
-from io import StringIO
 import pandas as pd
-from langchain.chains import ConversationChain
-from langchain.chains.llm import LLMChain
-from langchain.chains.router import MultiPromptChain
-from langchain.llms import OpenAI
-
-import tempfile
-
-
 from io import StringIO
-from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import ConversationChain, LLMChain
+from langchain.chains.router import MultiPromptChain, LLMRouterChain
+from langchain.chains.router.llm_router import RouterOutputParser
 
-# Make sure to add your OpenAI API key in the advanced settings of Streamlit's deployment
-open_AI_key = os.environ.get('OPENAI_API_KEY')
-openai.api_key = open_AI_key
 
-# Assuming you have the LangChain and financial route setup code here
-# ... (your existing LangChain and route setup code)
 
+Output_template = """
+ Format the output nicely into this template.
+
+------------
+ Summary
+------------
+
+ - Total savings:  {input}
+ - Monthly debt: {input}
+ - Monthly income: {input}
+
+------------
+ Financial situation:
+ ------------
+
+
+------------
+Recommendation:
+------------
+
+
+
+
+"""
+# Define your investment and debt templates (placeholders here)
+investment_template = """
+You are a highly knowledgeable investment advisor. You excel at providing clear and succinct advice on financial matters. When faced with a question you're unsure about, you honestly acknowledge your uncertainty.
+advise to invest  money and provide an investment portfolio based on savings and using 5 stocks.
+Give advice based on the following details: {input}
+
+""" + Output_template  # Replace with your actual investment template
+
+
+debt_template = debt_template= """
+You are a courteous and considerate debt advisor. Your approach involves tactfully and sensitively informing clients about their financial situations, avoiding any guilt-tripping. You then transition into planning mode, creating a strategy for clients to pay off their debts. This plan includes allocating 10% of their income for monthly debt payments.
+
+Here's the details provided {input}
+
+""" + Output_template  # Replace with your actual debt template
+
+def get_llm():
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+    llm = OpenAI(api_key=openai_api_key, model="text-davinci-003", temperature=0.8, max_tokens=150)
+    return llm
+
+def setup_financial_chains(llm, level):
+    fin_routes = [
+        {
+            "name": "InvestmentAdvisor",
+            "description": "Route for clients with a debt ratio less than 0.3.",
+            "prompt_template": investment_template
+        },
+        {
+            "name": "DebtAdvisor",
+            "description": "Route for clients with a debt ratio of 0.3 or higher.",
+            "prompt_template": debt_template
+        },
+    ]
+
+    destination_chains = {}
+    for p_info in fin_routes:
+        prompt = PromptTemplate(template=p_info["prompt_template"], input_variables=["input"])
+        chain = LLMChain(llm=llm, prompt=prompt)
+        destination_chains[p_info["name"]] = chain
+
+    financial_prompt = """
+    Hello! As your financial chatbot, I'm here to help you understand your current financial situation. 
+    Based on your financial knowledge, which you've indicated as {level}, I'll tailor my explanation to suit your understanding.
+
+    Now, let's summarize your financial status.
+    """
+
+    MULTI_PROMPT_ROUTER_TEMPLATE = """\
+    Given a raw text input to a language model select the model prompt best suited for \
+the input. You will be given the names of the available prompts and a description of \
+what the prompt is best suited for. You may also revise the original input if you \
+think that revising it will ultimately lead to a better response from the language \
+model.
+
+<< FORMATTING >>
+Return a markdown code snippet with a JSON object formatted to look like:
+```json
+{{{{
+    "destination": string \ name of the prompt to use or "DEFAULT"
+    "next_inputs": string \ a modified version of the original input. It is modified to contai only: the "savings" value, the "debt" value, the "income" value, and the "summary" provided above.
+}}}}
+```
+
+REMEMBER: "destination" MUST be one of the candidate prompt names specified below OR \
+it can be "DEFAULT" if the input is not well suited for any of the candidate prompts.
+REMEMBER: "next_inputs" is not the original input. It is modified to contain: the "savings" value, the "debt" value, the "income" value, and the "summary" provided above.
+
+<< CANDIDATE PROMPTS >>
+{destinations}
+
+<< INPUT >>
+{{input}}
+
+<< OUTPUT (must include ```json at the start of the response) >>
+<< OUTPUT (must end with ```) >>
+    ...  # Your MULTI_PROMPT_ROUTER_TEMPLATE content
+    """
+
+    prompt = financial_prompt + MULTI_PROMPT_ROUTER_TEMPLATE
+    destinations = [f"{route['name']}: {route['description']}" for route in fin_routes]
+    destinations_str = "\n".join(destinations)
+    router_template = prompt.format(destinations=destinations_str, level=level)
+
+    router_prompt = PromptTemplate(
+        template=router_template,
+        input_variables=["input"],
+        output_parser=RouterOutputParser(),
+    )
+    router_chain = LLMRouterChain.from_llm(llm, router_prompt)
+
+    return MultiPromptChain(
+        router_chain=router_chain,
+        destination_chains=destination_chains,
+        default_chain=ConversationChain(llm=llm, output_key="text"),
+        verbose=False,
+    )
 
 def loadCSVFile(uploaded_file):
-    
-
-    # Create a temporary file
-    tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.csv', mode='w+b')
-    # Write the uploaded file's content to the temporary file
-    tmp_file.write(uploaded_file.getvalue())
-    # Close the file to ensure it's saved
-    tmp_file_path = tmp_file.name
-    tmp_file.close()
-
-    # Now read the file from the temporary location
-    df = pd.read_csv(tmp_file_path)
-
-    # Clean up by deleting the temporary file
-    os.unlink(tmp_file_path)
-
-    # Convert DataFrame to string (or process as needed)
+    text_io = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    df = pd.read_csv(text_io)
     text = df.to_string(index=False)
     return text
 
-# Define the main function of the Streamlit app
+def run10times(csv_file, chain):
+    final_result = ""
+    for _ in range(10):
+        result = chain.run(csv_file)
+        final_result += result + "\n"
+    return final_result
+
+def process_financial_data(text, level):
+    for line in text.split('\n'):
+        if 'savings:' in line:
+            total_savings = line.split(':')[1].strip().replace('$', '').replace(',', '')
+        elif 'credit card debt:' in line:
+            monthly_debt = line.split(':')[1].strip().replace('$', '').replace(',', '')
+        elif 'income:' in line:
+            monthly_income = line.split(':')[1].strip().replace('$', '').replace(',', '')
+
+    total_savings = float(total_savings)
+    monthly_debt = float(monthly_debt)
+    monthly_income = float(monthly_income)
+
+    return total_savings, monthly_debt, monthly_income
+
 def main():
-    st.header("Welcome to FiniBot Financial Advisory! Please upload your financial spreadsheet. [Link to template]")
+    st.header("Welcome to FiniBot Financial Advisory! Please upload your financial spreadsheet.")
 
-    # File uploader for the spreadsheet
-    uploaded_file = st.file_uploader("Upload spreadsheet", type=['csv', 'xlsx'])
-    
-    # Radio button for selecting experience level
-    experience_level = st.radio("Select your experience level:", ("Novice", "Expert"))
+    level = st.radio("Select your experience level:", ("Novice", "Expert"))
+    uploaded_file = st.file_uploader("Upload your financial spreadsheet", type=['csv'])
 
-    # Display the spreadsheet if uploaded
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)  # Adjust this line if your spreadsheet is in a different format
-        st.dataframe(df)  # Displaying the spreadsheet
-
         text = loadCSVFile(uploaded_file)
+        total_savings, monthly_debt, monthly_income = process_financial_data(text, level)
 
-        print(text)
+        llm = get_llm()
+        financial_chain = setup_financial_chains(llm, level)
+        result = run10times(text, financial_chain)
 
-        # Process the spreadsheet and generate financial analysis
-        analysis, recommendation = process_spreadsheet(df, experience_level)
-        
-        # Display the analysis and recommendation
-        st.markdown("### FiniBot Analysis and Recommendation")
-        st.markdown(f"**Analysis:** {analysis}")
-        st.markdown(f"**Recommendation:** {recommendation}")
+        st.markdown("### Financial Summary")
+        st.markdown(f"- Total savings: ${total_savings}")
+        st.markdown(f"- Monthly debt: ${monthly_debt}")
+        st.markdown(f"- Monthly income: ${monthly_income}\n")
 
-# Function to process the spreadsheet and generate analysis and recommendation
-# Integrate the logic from your Jupyter notebook here.
-def process_spreadsheet(df, experience_level):
-    # Process the dataframe as per the logic in the Jupyter notebook
-    # Include LangChain and routing logic here
-    # ...
+        st.markdown("### Financial situation:")
+        st.markdown(f"  Financial level: {level}\n")
 
-    analysis = "Analysis based on the spreadsheet data."
-    recommendation = "Recommendation based on the analysis."
-    return analysis, recommendation
+        st.markdown("### Recommendation:")
+        st.markdown(result)
 
 if __name__ == "__main__":
     main()
